8a700ad62db1d5d071eb3ba0fbd5c62270ef1ef9
Michael Coulter <michael.coulter@arm.com>
2019-05-13 13:49:41 -0500

    Update TCL script and add circleci config file

--- synth-impl-bit.tcl
+++ synth-impl-bit.tcl
@@ -1,8 +1,8 @@
 source generate_project_basys3.tcl
 cd /home/vivado/project/project_basys3
-synth_design -top mbed_tester_basys3_top -part xc7a35tcpg236-1
-opt_design
-place_design
-route_design
-write_bitstream mbed_tester_basys3_top.bit
+synth_design -top mbed_tester_basys3_top -part xc7a35tcpg236-1 -verbose
+opt_design -verbose
+place_design -verbose
+route_design -verbose
+write_bitstream mbed_tester_basys3_top.bit -verbose
 exit
