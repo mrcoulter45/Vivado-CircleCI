6a298ab4ff74957328aa083d68b3e31bb633a740
Michael Coulter <michael.coulter@arm.com>
2019-05-14 14:02:38 -0500

    Add thread max of 4

--- synth-impl-bit.tcl
+++ synth-impl-bit.tcl
@@ -1,5 +1,6 @@
 source generate_project_basys3.tcl
 cd /home/vivado/project/project_basys3
+set_param general.maxThreads 4
 synth_design -top mbed_tester_basys3_top -part xc7a35tcpg236-1 -verbose
 opt_design -verbose
 place_design -verbose
